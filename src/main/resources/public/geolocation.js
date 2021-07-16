b428036abb1a12e842f8a2ba5a8cb76572b26c20
Ricky Xian <ricky_20768@hotmail.com>
2021-07-15 18:33:50 -0700

    got api to work

--- src/main/resources/public/geolocation.js
+++ src/main/resources/public/geolocation.js
@@ -39,10 +39,26 @@ option = {
     maximumAge: 0
 };
 
-
+/** 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: 100, lng: 100 },
       zoom: 8,
+    });
+  }
+  */
+
+  function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
     });
   }
