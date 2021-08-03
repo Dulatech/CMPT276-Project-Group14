eb645cc49c5d3a14555e0045145a030a4f481b34
JaspreetCS <jerryjagdev@gmail.com>
2021-08-03 13:34:01 -0700

    reservation tests

--- src/test/java/com/example/ReservationsTest.java
+++ src/test/java/com/example/ReservationsTest.java
@@ -10,9 +10,39 @@ class ReservationTest {
 
     @BeforeAll
     static void setUp() {
-        
+        r = new Reservations();
+        r.setUserID(49);
+        r.setRestaurantID(3);
+        r.setID(2);
+        r.setPhone("215646514");
     }
 
+    @Test
+    public void UserIDTest() {
+        assertEquals(49, r.getUserID());
+        r.setUserID(23);
+        assertEquals(23, r.getUserID());
+    }
 
+    @Test 
+    public void restaurantIDTest() {
+        assertEquals(3, r.getRestaurantID());
+        r.setRestaurantID(54);
+        assertEquals(54, r.getRestaurantID());
+    }
+
+    @Test
+    public void idTest() {
+        assertEquals(2, r.getID());
+        r.setID(15);
+        assertEquals(15, r.getID());
+    }
+
+    @Test
+    public void PhoneTest() {
+        assertEquals("215646514", r.getPhone());
+        r.setPhone("6245211554");
+        assertEquals("6245211554", r.getPhone());
+    }
     
 }
