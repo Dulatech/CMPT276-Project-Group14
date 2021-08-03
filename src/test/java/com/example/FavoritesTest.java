806036b40fad03fb2512e7b4a66b99e9195862ba
Ricky Xian <ricky_20768@hotmail.com>
2021-08-02 19:31:07 -0700

    user and favorite test

--- src/test/java/com/example/FavoritesTest.java
+++ src/test/java/com/example/FavoritesTest.java
@@ -17,10 +17,24 @@ class FavoritesTest {
     }
 
     @Test
-    public void UserID() {
+    public void UserIDTest() {
         assertEquals(1, f.getUserID());
         f.setUserID(23);
         assertEquals(23, f.getUserID());
+    }
+
+    @Test 
+    public void restaurantIDTest() {
+        assertEquals(2, f.getRestaurantID());
+        f.setRestaurantID(69);
+        assertEquals(69, f.getRestaurantID());
+    }
+
+    @Test
+    public void idTest() {
+        assertEquals(3, f.getID());
+        f.setID(19);
+        assertEquals(19, f.getID());
     }
 }
 
