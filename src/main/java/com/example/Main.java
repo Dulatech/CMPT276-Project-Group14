b375432f0d2e07628ace630d55c50c65c2b79ae2
PhilipZhangs <73215906+PhilipZhangs@users.noreply.github.com>
2021-08-03 20:53:55 -0700

    error

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1389,15 +1389,15 @@ public String restTerminal(Map<String, Object> model, @ModelAttribute("userID") 
   String reviewHome(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+
+      ArrayList<Reviews> output = new ArrayList<Reviews>();
       ResultSet rs1 = stmt.executeQuery("SELECT * FROM Reviews WHERE userID=" + userID);
       while (r1s.next()) {
         String name = rs.getString("Name");
         String comment = rs.getString("Comment");
         Integer rating = rs.getInt("Rating");
-        
-        
-  }
+      }
 
+    }
+  }
 }
